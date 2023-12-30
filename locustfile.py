bf6dae77deae4a083d5b8ca2198c2ba9ec0b027d
Hamza <jadalaou@eurecom.fr>
2023-12-30 19:42:25 +0100

    commits

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,6 @@
-from locust import HttpUser, task, between
+from locustfile import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2.5)
-    host = "http://20.232.41.73:5000"  
-
     @task
     def invoke_microservice(self):
         self.client.get("/numericalintegralservice/0.0/3.14")
