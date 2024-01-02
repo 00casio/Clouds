0557e4f0fc3b0056b148dab9bf231699a46b729b
Hamza <jadalaou@eurecom.fr>
2024-01-02 22:22:18 +0100

    added files

--- locustfile.py
+++ locustfile.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class QuickstartUser(HttpUser):
     @task
     def invoke_microservice(self):
-        self.client.get("/numericalintegralservice/0.0/3.14")
+        self.client.get("/numericalintegralservice/0.0/3000.14")
