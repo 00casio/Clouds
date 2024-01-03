a55b9c642ee1339964a0efd8177a7c035bf9d4e5
Hamza <jadalaou@eurecom.fr>
2024-01-03 17:37:47 +0100

    locust test for task4

--- locustfile.py
+++ locustfile.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class QuickstartUser(HttpUser):
     @task
     def invoke_microservice(self):
-        self.client.get("/numericalintegralservice/0.0/3000.14")
+        self.client.get("/api/http_trigger?lower=0.0&upper=1000")
